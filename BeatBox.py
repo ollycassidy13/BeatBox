893641bf128fbbabd74dc3ba792eae3aa592309f
ollycassidy13 <ollyj.cassidy@gmail.com>
2024-07-06 22:45:08 +0100

    New improved GUI

--- BeatBox.py
+++ BeatBox.py
@@ -1,8 +1,8 @@
 import pygame
 import os
-from tkinter import *
-from tkinter import filedialog
-from tkinter import ttk
+import customtkinter as ctk
+from customtkinter import CTkImage
+from tkinter import filedialog, Listbox, LEFT, BOTH, Y, RIGHT, END
 from PIL import Image, ImageTk
 import time
 
@@ -12,85 +12,98 @@ class MusicPlayer:
     def __init__(self, root):
         self.root = root
         self.root.title("BeatBox")
-        self.root.minsize(800, 580)
-        self.root.configure(bg='#ffffff')
-        
-        style = ttk.Style()
-        style.configure('TButton', background='#d3d3d3', foreground='#333333', font=('Helvetica', 12))
-        style.map('TButton', background=[('active', '#b0b0b0')])
-        style.configure('TLabel', background='#ffffff', foreground='#333333', font=('Helvetica', 12))
-        style.configure('TListbox', background='#f0f0f0', foreground='#333333', font=('Helvetica', 12))
-        style.configure('TProgressbar', troughcolor='#ffffff', background='#333333')
+        self.root.minsize(1200, 580)
+        ctk.set_appearance_mode("dark")
 
-        # Track frame
-        self.track_frame = Frame(self.root, bg="#ffffff")
-        self.track_frame.place(relx=0.0125, rely=0.025, relwidth=0.975, relheight=0.25)
-        
-        # Track frame label
-        self.track_label = ttk.Label(self.root, text="Current Track", font=("Helvetica", 12, "bold"), background="#ffffff", foreground="#333333")
-        self.track_label.place(relx=0.0225, rely=0.008)
-        
-        # Track name
-        self.track_name_var = StringVar()
-        self.track_name = ttk.Label(self.track_frame, textvariable=self.track_name_var, font=("Helvetica", 16), background="#ffffff")
+        # determine the directory of the current script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        # path to the custom theme file
+        custom_theme_path = os.path.join(script_dir, "theme1.json")
+        # set the custom theme
+        ctk.set_default_color_theme(custom_theme_path)
+
+        # track frame
+        self.track_frame = ctk.CTkFrame(self.root)
+        self.track_frame.place(relx=0.0125, rely=0.03, relwidth=0.975, relheight=0.225)
+
+        # track frame label
+        self.track_label = ctk.CTkLabel(self.root, text="Current Track", font=("Helvetica", 12, "bold"))
+        self.track_label.place(relx=0.02, rely=0.008)
+
+        # track name
+        self.track_name_var = ctk.StringVar()
+        self.track_name = ctk.CTkLabel(self.track_frame, textvariable=self.track_name_var, font=("Helvetica", 16))
         self.track_name.place(relx=0.225, rely=0.1)
 
-        # Album cover
-        self.album_cover_label = Label(self.track_frame, bg='#ffffff')
+        # album cover
+        self.album_cover_label = ctk.CTkLabel(self.track_frame, text="No Album Cover")
         self.album_cover_label.place(relx=0.0125, rely=0.2, relwidth=0.1875, relheight=0.8)
-        
-        # Progress bar
-        self.progress = ttk.Progressbar(self.track_frame, orient=HORIZONTAL, length=100, mode='determinate')
+
+        # progress bar
+        self.progress = ctk.CTkProgressBar(self.track_frame)
         self.progress.place(relx=0.225, rely=0.6, relwidth=0.75, relheight=0.2)
 
-        # Time labels
-        self.elapsed_time_label = ttk.Label(self.track_frame, text="0:00", font=("Helvetica", 10), background="#ffffff", foreground="#333333")
+        # time labels
+        self.elapsed_time_label = ctk.CTkLabel(self.track_frame, text="0:00", font=("Helvetica", 10))
         self.elapsed_time_label.place(relx=0.225, rely=0.8)
 
-        self.remaining_time_label = ttk.Label(self.track_frame, text="0:00", font=("Helvetica", 10), background="#ffffff", foreground="#333333")
+        self.remaining_time_label = ctk.CTkLabel(self.track_frame, text="0:00", font=("Helvetica", 10))
         self.remaining_time_label.place(relx=0.975, rely=0.8, anchor='ne')
 
-        # Control frame
-        self.control_frame = Frame(self.root, bg="#ffffff")
-        self.control_frame.place(relx=0.0125, rely=0.33, relwidth=0.975, relheight=0.1)
-        
-        # Control frame label
-        self.control_label = ttk.Label(self.root, text="Control Panel", font=("Helvetica", 12, "bold"), background="#ffffff", foreground="#333333")
-        self.control_label.place(relx=0.025, rely=0.30)
-        
-        # Control buttons
-        self.load_button = ttk.Button(self.control_frame, text="Load Folder", command=self.load_folder)
+        # control frame
+        self.control_frame = ctk.CTkFrame(self.root)
+        self.control_frame.place(relx=0.0125, rely=0.33, relwidth=0.8, relheight=0.1)
+
+        # control frame label
+        self.control_label = ctk.CTkLabel(self.root, text="Controls", font=("Helvetica", 12, "bold"))
+        self.control_label.place(relx=0.025, rely=0.29)
+
+        # control buttons
+        self.load_button = ctk.CTkButton(self.control_frame, text="Load Folder", command=self.load_folder)
         self.load_button.grid(row=0, column=0, padx=10, pady=10)
-        
-        self.play_button = ttk.Button(self.control_frame, text="▶", command=self.play_song)
+
+        self.play_button = ctk.CTkButton(self.control_frame, text="▶", command=self.play_song)
         self.play_button.grid(row=0, column=1, padx=10, pady=10)
-        
-        self.pause_button = ttk.Button(self.control_frame, text="||", command=self.pause_song)
+
+        self.pause_button = ctk.CTkButton(self.control_frame, text="||", command=self.pause_song)
         self.pause_button.grid(row=0, column=2, padx=10, pady=10)
-        
-        self.stop_button = ttk.Button(self.control_frame, text="■", command=self.stop_song)
+
+        self.stop_button = ctk.CTkButton(self.control_frame, text="■", command=self.stop_song)
         self.stop_button.grid(row=0, column=3, padx=10, pady=10)
 
-        self.skip_button = ttk.Button(self.control_frame, text=">|", command=self.skip_song)
+        self.skip_button = ctk.CTkButton(self.control_frame, text=">|", command=self.skip_song)
         self.skip_button.grid(row=0, column=4, padx=10, pady=10)
 
-        self.remove_button = ttk.Button(self.control_frame, text="Remove Song", command=self.remove_songs)
+        self.remove_button = ctk.CTkButton(self.control_frame, text="Remove Song", command=self.remove_songs)
         self.remove_button.grid(row=0, column=5, padx=10, pady=10)
-        
-        # Listbox frame
-        self.listbox_frame = Frame(self.root, bg="#ffffff")
+
+        # spinning vinyl frame
+        self.vinyl_frame = ctk.CTkFrame(self.root)
+        self.vinyl_frame.place(relx=0.82, rely=0.33, relwidth=0.1, relheight=0.225)
+
+        # vinyl image
+        self.vinyl_image_path = os.path.join(script_dir, "vinyl2.jpg")
+        self.vinyl_image = Image.open(self.vinyl_image_path)
+        self.vinyl_image = self.vinyl_image.resize((120, 120), Image.LANCZOS)
+        self.vinyl_image = ImageTk.PhotoImage(self.vinyl_image)
+        self.vinyl_label = ctk.CTkLabel(self.vinyl_frame, image=self.vinyl_image)
+        self.vinyl_label.image = self.vinyl_image
+        self.vinyl_label.place(relx=0.5, rely=0.33, anchor='center')
+
+        # listbox frame
+        self.listbox_frame = ctk.CTkFrame(self.root)
         self.listbox_frame.place(relx=0.0125, rely=0.49, relwidth=0.975, relheight=0.48)
-        
-        # Listbox frame label
-        self.queue_label = ttk.Label(self.root, text="Queue", font=("Helvetica", 12, "bold"), background="#ffffff", foreground="#333333")
+
+        # listbox frame label
+        self.queue_label = ctk.CTkLabel(self.root, text="Queue", font=("Helvetica", 12, "bold"))
         self.queue_label.place(relx=0.025, rely=0.45)
-        
-        # Listbox for songs loaded
-        self.queue = Listbox(self.listbox_frame, selectbackground="gold", selectmode=SINGLE, font=("Helvetica", 12), bg="#f0f0f0", fg="#333333")
+
+        # listbox for songs loaded
+        self.queue = Listbox(self.listbox_frame, font=("Helvetica", 12), bg="black", fg="gray", selectbackground="gray", selectforeground="black")
         self.queue.pack(side=LEFT, fill=BOTH, expand=True)
-        
-        # Scrollbar
-        self.scrollbar = ttk.Scrollbar(self.listbox_frame, orient=VERTICAL, command=self.queue.yview)
+
+        # scrollbar
+        self.scrollbar = ctk.CTkScrollbar(self.listbox_frame, command=self.queue.yview)
         self.scrollbar.pack(side=RIGHT, fill=Y)
         self.queue.config(yscrollcommand=self.scrollbar.set)
 
@@ -102,9 +115,13 @@ class MusicPlayer:
         self.stopped = False
         self.selected_index = 0
 
-        # Check for end of track event
+        # check for end of track event
         self.check_music_end()
 
+        # Start spinning vinyl record animation
+        self.angle = 0
+        self.update_progress()  # Start updating the progress bar
+        
     def load_folder(self):
         folder_path = filedialog.askdirectory()
         if folder_path:
@@ -126,10 +143,9 @@ class MusicPlayer:
     def display_album_cover(self, image_path):
         img = Image.open(image_path)
         img = img.resize((150, 150), Image.LANCZOS)
-        self.album_cover = ImageTk.PhotoImage(img)
-        self.album_cover_label.config(image=self.album_cover)
-        self.album_cover_label.config(width=150, height=150)
-
+        self.album_cover = CTkImage(light_image=img, dark_image=img, size=(150, 150))
+        self.album_cover_label.configure(image=self.album_cover, text="")
+        
     def remove_songs(self):
         selected_indices = self.queue.curselection()
         if selected_indices:
@@ -140,10 +156,12 @@ class MusicPlayer:
             del self.folder_names[selected_index]
             if song_file in self.song_to_cover:
                 del self.song_to_cover[song_file]
-        
+
     def play_song(self):
+        # if stopped play current song from beginning
         if self.stopped:
             self.stopped = False
+            # if a different song selected play that song from beginning
             if self.queue.curselection():
                 self.selected_index = self.queue.curselection()[0]
             else:
@@ -156,6 +174,7 @@ class MusicPlayer:
             if song_file in self.song_to_cover:
                 self.display_album_cover(self.song_to_cover[song_file])
             self.update_progress_bar()
+        # if paused play current song from paused place
         elif self.is_paused and not self.stopped:
             pygame.mixer.music.unpause()
             self.is_paused = False
@@ -163,10 +182,12 @@ class MusicPlayer:
         else:
             if self.queue.curselection():
                 self.selected_index = self.queue.curselection()[0]
+            # play first song in queue
             else:
                 self.selected_index = 0
                 self.queue.select_set(self.selected_index)
-            
+
+            # play the music
             if self.selected_index < len(self.song_files):
                 self.current_song = self.queue.get(self.selected_index)
                 self.track_name_var.set(self.folder_names[self.selected_index])
@@ -179,18 +200,18 @@ class MusicPlayer:
                 self.update_progress_bar()
 
         self.queue.selection_clear(0, END)
-        
+
     def pause_song(self):
         pygame.mixer.music.pause()
         self.is_paused = True
-        
+
     def stop_song(self):
         pygame.mixer.music.stop()
         self.is_paused = False
         self.stopped = True
-        self.progress["value"] = 0
-        self.elapsed_time_label.config(text="0:00")
-        self.remaining_time_label.config(text=f"-{self.format_time(self.progress['maximum'])}")
+        self.progress.set(0)
+        self.elapsed_time_label.configure(text="0:00")
+        self.remaining_time_label.configure(text=f"-{self.format_time(self.progress.get())}")
 
     def skip_song(self):
         if self.selected_index + 1 < len(self.song_files):
@@ -217,21 +238,29 @@ class MusicPlayer:
             if song_file in self.song_to_cover:
                 self.display_album_cover(self.song_to_cover[song_file])
             self.update_progress_bar()
-    
+
     def update_progress_bar(self):
         song_length = pygame.mixer.Sound(self.song_files[self.selected_index]).get_length()
-        self.progress["maximum"] = song_length
-        self.update_progress()
+        self.progress.set(0)
+        self.progress_length = song_length
+        self.update_progress()  # start updating progress bar
 
     def update_progress(self):
         if pygame.mixer.music.get_busy() and not self.stopped:
             current_time = pygame.mixer.music.get_pos() / 1000
-            song_length = self.progress["maximum"]
-            self.progress["value"] = current_time
-            self.elapsed_time_label.config(text=self.format_time(current_time))
-            self.remaining_time_label.config(text=f"-{self.format_time(song_length - current_time)}")
-            self.root.after(1000, self.update_progress)
-    
+            self.progress.set(current_time / self.progress_length)
+            self.elapsed_time_label.configure(text=self.format_time(current_time))
+            self.remaining_time_label.configure(text=f"-{self.format_time(self.progress_length - current_time)}")
+            if not self.stopped and self.song_files:
+                self.angle += 5
+                img = Image.open(self.vinyl_image_path)
+                img = img.resize((150, 150), Image.LANCZOS)
+                img = img.rotate(self.angle)
+                self.vinyl_image = CTkImage(light_image=img, dark_image=img, size=(150, 150))
+                self.vinyl_label.configure(image=self.vinyl_image)
+                self.vinyl_label.image = self.vinyl_image
+            self.root.after(1000, self.update_progress)  # keep updating progress 
+
     def format_time(self, seconds):
         minutes = int(seconds // 60)
         seconds = int(seconds % 60)
@@ -242,6 +271,6 @@ class MusicPlayer:
             self.skip_song()
         self.root.after(100, self.check_music_end)
 
-root = Tk()
+root = ctk.CTk()
 app = MusicPlayer(root)
 root.mainloop()
